22bfafb227b2aa711eeed8ca535c98ba94dc03fd
0010200303 <55239897+0010200303@users.noreply.github.com>
2020-04-02 00:04:43 +0200

    Everthing
    Just everything so far

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# LanguagePack
-Create a language for Mount &amp; Blade Bannerlord
+Install:
+Just drag GUI into the main Folder of the Game (Mount & Blade II Bannerlord)
+
+Add Language:
+Add the language_id in "Mount & Blade II Bannerlord\GUI\GauntletUI\Fonts\Languages.xml" and a new folder with all localization xmls and the the language_id there
---
+++ SubModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace LanguagePack
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            // add the change language button
+            TaleWorlds.MountAndBlade.Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Reload_Languages", new TextObject("Reload Languages", null), 9999999, () => Reset_gameTextDictionary(), false));
+        }
+
+        public static void Reset_gameTextDictionary()
+        {
+            // clear the translation data from the game using reflection
+            Type type = typeof(LocalizedTextManager);
+            FieldInfo info = type.GetField("_gameTextDictionary", BindingFlags.NonPublic | BindingFlags.Static);
+            object value = info.GetValue(null);
+
+            value.GetType().GetMethod("Clear").Invoke(value, null);
+
+            // reload all localization xmls
+            LocalizedTextManager.LoadLocalizationXmls();
+        }
+    }
+}
